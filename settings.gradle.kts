a6308500ba4e6d21ce38008ebb13c275417a67ec
zigqora <zigqora@users.noreply.github.com>
2026-08-11 14:41:15 +0530

    Clean up unused stonecutter template files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,20 +4,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven("https://maven.fabricmc.net/")
-        maven("https://maven.kikugie.dev/releases") { name = "KikuGie Releases" }
-        maven("https://maven.kikugie.dev/snapshots") { name = "KikuGie Snapshots" }
-    }
-}
-
-plugins {
-    id("dev.kikugie.stonecutter") version "0.9.5"
-    id("dev.kikugie.loom-back-compat") version "0.3"
-}
-
-stonecutter {
-    create(rootProject) {
-        versions("1.21.1", "1.21.3", "1.21.4", "1.21.11")
-        vcsVersion = "1.21.1"
     }
 }
 
